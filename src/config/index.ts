c52015771e175c860b13a248a920e3ba21e4f300
dmystical-coder <dmystical.coder@gmail.com>
2025-11-25 23:02:26 +0100

    feat(web3): create metadata object for AppKit

--- src/config/index.ts
+++ src/config/index.ts
@@ -6,3 +6,10 @@ export const PROJECT_ID = process.env.NEXT_PUBLIC_PROJECT_ID;
 if (!PROJECT_ID) {
   throw new Error('Project ID is not defined');
 }
+
+export const metadata = {
+  name: 'GigLink',
+  description: 'Decentralized Micro-Bounties for the Atomic Economy',
+  url: 'https://giglink.vercel.app', // origin must match your domain & subdomain
+  icons: ['https://avatars.githubusercontent.com/u/179229932']
+};
